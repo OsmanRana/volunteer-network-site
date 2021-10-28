de53084d8c9325bb599a9ab8cafff22d668294e0
OsmanRana <osmancmc@yahoo.com>
2021-10-28 11:35:52 +0600

    sigle event loaded

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { useForm } from "react-hook-form";
 
@@ -6,6 +6,16 @@ import { useForm } from "react-hook-form";
 const Register = () => {
     
     const {id} = useParams()
+    // getting single event data
+    const [event, setEvent] = useState();
+    useEffect(()=>{
+        const url = `http://localhost:5000/events/${id}`
+        fetch(url)
+        .then(res=>res.json())
+        .then(data=> setEvent(data))
+    },[])
+
+    // sending user data to the database
     const { register, handleSubmit , reset} = useForm();
     const onSubmit = data => {
         fetch('http://localhost:5000/users',{
@@ -26,7 +36,7 @@ const Register = () => {
     return (
         <div className="container my-5">
             <Link to='/home'><img className="img-fluid mx-auto d-block w-25" src="/Group 1329.png" alt="" /></Link>
-            {id}
+            {/* {id} */}
             <div className=" shadow my-5  p-5 login-box border rounded-3">
                 <h3 className="ms-5">Register as a Volunteer</h3>
                 <form className="ms-5 d-flex flex-column my-3" onSubmit={handleSubmit(onSubmit)}>
@@ -34,7 +44,7 @@ const Register = () => {
                     <input className="my-3 py-3 border-0 border-bottom" placeholder="Email" {...register("email", { required: true })} />
                     <input className="my-3 py-3 border-0 border-bottom" placeholder="Date" type="date" {...register("date", { required: true })} />
                     <input className="my-3 py-3 border-0 border-bottom" placeholder="Description" {...register("description", { required: true })} />
-                    <input className="my-3 py-3 border-0 border-bottom" placeholder="Event" defaultValue={''} {...register("event", { required: true })} />
+                    <input className="my-3 py-3 border-0 border-bottom" placeholder="Event" defaultValue={event?.title} {...register("event", { required: true })} />
                     <input className="btn btn-primary py-3" type="submit" value="Register" />
                 </form>
                 <p className="ms-5">Already registered? <Link to='/login'>Sign In here</Link></p>
